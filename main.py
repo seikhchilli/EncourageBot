1a5174510dc718e2b54db69530d6ad3e07a29453
Saurabh Kumar <saurabhkrsingh2001@gmail.com>
2022-02-01 07:57:29 +0000

    youtube player and guess my birthday

--- main.py
+++ main.py
@@ -4,75 +4,212 @@ import requests
 import json
 import random
 from replit import db
+from keep_alive import keep_alive
+from discord.ext import commands,tasks
+from pytube import YouTube
+from pytube import Search
+import pafy
+import asyncio
+from discord import FFmpegPCMAudio
 
-client = discord.Client()
 
-sad_words = ["sad", "depressed", "depressing", "unhappy", "angry", "miserable"]
+bot = commands.Bot(command_prefix = '//')
 
-starter_encouragements = ["Cheer Up!", "Hang in There.", "You are a great person"]
+check = False
 
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return quote
+@bot.command(name = "hello")
+async def hello(ctx):
+  await ctx.send("Hello {}".format(ctx.author))
 
-def update_encouragements(encouraging_message):
-  if "encouragements" in db.keys():
-    encouragements = db["encouragements"]
-    encouragements.append(encouraging_message)
-    db["encouragements"] = encouragements
+@bot.command(name = "kill")
+async def kill(ctx, args):
+  await ctx.send("{} has beeen eliminated".format(args))
+
+@bot.command(name = "speechless")
+async def speechless(ctx):
+  await ctx.send(file=discord.File('speechless.gif'))
+
+@bot.command(name = "UPcm")
+async def upcm(ctx):
+  await ctx.send(file=discord.File('upcm.jpg'))
+
+@bot.command(name = "play" ,aliases=["p"])
+async def play(ctx, *args):
+
+  global check
+  
+  global s
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+  if voice == None:
+    if not ctx.message.author.voice:
+      await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+      return
+    else:
+      channel = ctx.message.author.voice.channel
+      await channel.connect()
+    
+  server = ctx.message.guild
+  vc = server.voice_client
+  
+
+
+  
+  if not check:
+    check = not check
+    s = Search(' '.join(args))
+    j = 1
+    output = "**Please select a track with the** `//play 1-5` **command:**\n"
+    for i in s.results:
+      title = (i.title).encode('utf8')
+      output += '**' + str(j) + ': **'
+      output += (title.decode('utf8')) + '\n'
+      if j == 5:
+        break
+      j += 1
+    
+    global message
+    message = await ctx.send(output)
+
+
+
   else:
-    db["encouragements"] = [encouraging_message]
+    if not args[0].isnumeric():
+      await ctx.send("Choose index number of song.")
+      return
 
-def delete_encouragements(index):
-  encouragements = db["encouragements"]
-  if len(encouragements) > index:
-    del encouragements[index]
-    db["encouragements"] = encouragements
+    check = not check
+    if vc.is_playing():
+      vc.stop()
+    vdo_index = eval(args[0])
+    vdo_id = s.results[vdo_index-1].video_id
+
+    p = pafy.new(vdo_id)
+    ba = p.getbestaudio()
 
 
 
+    try :
 
-@client.event
-async def on_ready():
-  print("We have logged in as {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-  msg = message.content
-  if message.author == client.user:
+      async with ctx.typing():
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        
+        vc.play(FFmpegPCMAudio(ba.url, **FFMPEG_OPTIONS))
+        vc.is_playing()
+
+        await message.edit(content ='**Now playing:** {}'.format(p.title))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.pause()
+        await ctx.send("Paused. Use resume command to resume.")
+
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+    
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        voice_client.resume()
+        await ctx.send("Resumed")
+
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.stop()
+        await ctx.send("Stopped")
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@bot.command(name = 'guessmybday', help = "Guesses your birthday")
+async def guessmybday(ctx):
+  count = 0
+  msg = await ctx.send("**Is your birthday in the following set?**\n 1  3   5   7\n 9  11  13  15\n17  19  21  23\n25  27  29 31\n")
+
+  check = lambda m: m.author == ctx.author and m.channel == ctx.channel
+
+  try:
+    confirm = await bot.wait_for("message", check=check, timeout=60)
+  except asyncio.TimeoutError:
+    await msg.edit(content="Guessing cancelled, timed out.")
+    return
+
+  if confirm.content == "yes" or confirm.content == "no":
+    if confirm.content == "yes":
+      count += 1
+    await msg.edit(content = "**Is your birthday in the following set?**\n 2   3   6   7\n10  11  14  15\n18  19 22  23\n26  27  30  31\n")
+    
+  
+  check = lambda m: m.author == ctx.author and m.channel == ctx.channel
+
+  try:
+    confirm = await bot.wait_for("message", check=check, timeout=60)
+  except asyncio.TimeoutError:
+    await msg.edit(content="Guessing cancelled, timed out.")
     return
   
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  if confirm.content == "yes" or confirm.content == "no":
+    if confirm.content == "yes":
+      count += 2
+    await msg.edit(content = "**Is your birthday in the following set?**\n 4   5   6   7\n12  13  14  15\n20  21  22  23\n28  29  30  31\n")
   
-  if message.content.startswith('$inspire'):
-    await message.channel.send(get_quote())
+  check = lambda m: m.author == ctx.author and m.channel == ctx.channel
 
-
-  options = starter_encouragements
-  if "encouragements" in db.keys():
-     options.extend(db["encouragements"])
-
-
-  if any(word in msg for word in sad_words):
-    await message.channel.send(random.choice(options))
-
-  if msg.startswith("$new"):
-    encouraging_message = msg.split("$new ", 1)[1]
-    update_encouragements(encouraging_message)
-    await message.channel.send("New Encouraging Message Added.")
+  try:
+    confirm = await bot.wait_for("message", check=check, timeout=60)
+  except asyncio.TimeoutError:
+    await msg.edit(content="Guessing cancelled, timed out.")
+    return
   
-  if msg.startswith("$del"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      index = int(msg.split("$del", 1)[1])
-      delete_encouragements(index)
-      encouragements = db["encouragements"]
-    
-    await message.channel.send(encouragements)
-
-
+  if confirm.content == "yes" or confirm.content == "no":
+    if confirm.content == "yes":
+      count += 4
+    await msg.edit(content = "**Is your birthday in the following set?**\n 8  9  10  11\n12  13  14  15\n24  25  26  27\n28  29 30  31\n")
   
-client.run(os.getenv('TOKEN'))
+  check = lambda m: m.author == ctx.author and m.channel == ctx.channel
+
+  try:
+    confirm = await bot.wait_for("message", check=check, timeout=60)
+  except asyncio.TimeoutError:
+    await msg.edit(content="Guessing cancelled, timed out.")
+    return
+  
+  if confirm.content == "yes" or confirm.content == "no":
+    if confirm.content == "yes":
+      count += 8
+    await msg.edit(content = "**Is your birthday in the following set?**\n16  17  18  19\n20  21  22  23\n24  25  26  27\n28  29  30  31\n")
+
+  check = lambda m: m.author == ctx.author and m.channel == ctx.channel
+
+  try:
+    confirm = await bot.wait_for("message", check=check, timeout=60)
+  except asyncio.TimeoutError:
+    await msg.edit(content="Guessing cancelled, timed out.")
+    return
+
+  if confirm.content == "yes" or confirm.content == "no":
+    if confirm.content == "yes":
+      count += 16
+    await msg.edit(content = "Your Birthday is on **" + str(count) + "**")
+  return
+
+
+
+
+
+
+keep_alive() 
+bot.run(os.getenv('TOKEN'))
